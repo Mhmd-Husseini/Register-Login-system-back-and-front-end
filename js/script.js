f5a9eb5fe7b3e31248b6f5e11f6a5086c198472b
Husseini <mhmd.husseinyy@gmail.com>
2023-07-21 12:50:57 +0300

    adding validation to signup function in js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,35 @@
+function validateSignUpForm() {
+  const username = document.getElementById("signup_username").value;
+  const password = document.getElementById("signup_password").value;
+  const firstName = document.getElementById("signup_fname").value;
+  const lastName = document.getElementById("signup_lname").value;
+
+  if (!username || !password || !firstName || !lastName) {
+    alert("Please provide all required fields.");
+    return false;
+  }
+  if (password.length < 8) {
+    alert("Password must be at least 8 characters long.");
+    return false;
+  }
+  return true;
+}
+
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("signup").addEventListener("submit", function(event) {
       event.preventDefault();
-  
-      fetch("http://127.0.0.1/Register-Login-system-back-and-front-end/apis/signup.php", {
-        method: "POST",
-        body: new FormData(event.target)
-      })
-      .then((response) => response.json())
-      .then((data) => {
-        alert(data.message); 
-      })
-      .catch((error) => console.log(error));
-    });
-
+      if (validateSignUpForm()) {
+          fetch("http://127.0.0.1/Register-Login-system-back-and-front-end/apis/signup.php", {
+            method: "POST",
+            body: new FormData(event.target)
+          })
+          .then((response) => response.json())
+          .then((data) => {
+            alert(data.message); 
+          })
+          .catch((error) => console.log(error));
+        };
+      });
     document.getElementById("signin").addEventListener("submit", function(event) {
         event.preventDefault();
       
